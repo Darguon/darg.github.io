7c39d2ef95e41f761080620ae51c1e6b3a0828b1
Darguon <158139399+Darguon@users.noreply.github.com>
2024-08-07 21:35:26 +0300

    Pause Button

--- script.js
+++ script.js
@@ -177,3 +177,32 @@ document.addEventListener("DOMContentLoaded", function() {
     }, fullTitle.length * typingSpeed * 2 + pauseBetweenCycles * 2);
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const enterScreen = document.getElementById("enter-screen");
+    const myVideo = document.getElementById("myVideo");
+    const backgroundMusic = document.getElementById("background-music");
+    const profile = document.getElementById("profile");
+    const pauseButton = document.getElementById("pause-button");
+    const pauseIcon = document.getElementById("pause-icon");
+
+    enterScreen.addEventListener("click", function(event) {
+        event.preventDefault();
+        enterScreen.style.display = "none";
+        myVideo.style.display = "block";
+        profile.style.display = "block";
+        pauseButton.style.display = "block";
+        backgroundMusic.play();
+    });
+
+    pauseButton.addEventListener("click", function() {
+        if (myVideo.paused) {
+            myVideo.play();
+            backgroundMusic.play();
+            pauseIcon.innerHTML = '<path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80L0 432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/>';
+        } else {
+            myVideo.pause();
+            backgroundMusic.pause();
+            pauseIcon.innerHTML = '<path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80L0 432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/>';
+        }
+    });
+});
